a033575384c2003ab339d118056b79ef75128f65
Unknown <daan.van.hauwermeiren@gmail.com>
2019-10-09 00:22:50 +0200

    add some stuff
    fooling around

--- src/testJuliaPackage.jl
+++ src/testJuliaPackage.jl
@@ -2,4 +2,12 @@ module testJuliaPackage
 
 greet() = print("Hello World!")
 
+include("extra_file.jl")
+
+# make this available for usage if the package is loaded in by
+# using testJuliaPackage
+# so only export functions which are of interest to users,
+# keep the others internal
+export my_f
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using testJuliaPackage
 using Test
 
+#without export
+#testJuliaPackage.my_f(2,1)
+# by using export, this is available, otherwith an UndefVarError is thrown
+#my_f(2,1)
+
+
 @testset "testJuliaPackage.jl" begin
-    # Write your own tests here.
+    #2x + 3y
+    @test my_f(2,1) == 7
+    @test my_f(2,3) == 13
 end
